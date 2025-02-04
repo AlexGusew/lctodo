9074cbaed951b062d7141e3a6f1463ecaf02d1d0
Alex Gusev <sasha19755791@gmail.com>
2025-02-04 08:26:21 +0000

    Enhance DifficultyChip component styles for better visibility in dark mode

--- src/components/ui/DifficultyChip.tsx
+++ src/components/ui/DifficultyChip.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 const getBackgroundColorClass = (difficulty: Question["difficulty"]) =>
   ({
-    Easy: "bg-green-500",
-    Medium: "bg-orange-500",
-    Hard: "bg-rose-500",
+    Easy: "dark:bg-green-700 bg-green-500",
+    Medium: "dark:bg-orange-600 bg-orange-500",
+    Hard: "dark:bg-rose-700 bg-rose-500",
   }[difficulty] ?? "bg-gray-500");
 
 export const DifficultyChip = ({
@@ -18,7 +18,7 @@ export const DifficultyChip = ({
     <span
       className={cn(
         getBackgroundColorClass(difficulty),
-        "text-zinc-900 px-2 rounded-full py-0.5 text-xs flex items-center justify-center"
+        "text-white px-2 rounded-full py-0.5 text-xs flex items-center justify-center"
       )}
     >
       {difficulty}
